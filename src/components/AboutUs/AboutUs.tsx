47d150d2e928a1dd9906a011be6f9132eae55b28
Igor Bondarenko <igorbondarenko35@gmail.com>
2023-04-24 22:41:44 +0300

    changed object position, makes breakpoints 1024, 1550 and 1920 for AboutUs section and added animation plug

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -6,6 +6,8 @@ import dot1 from '../../images/dots/dot1.webp';
 import dot2 from '../../images/dots/dot2.webp';
 import dot3 from '../../images/dots/dot3.webp';
 
+import Plug from '../../images/animation.png';
+
 export const AboutUs: React.FC = () => {
   const [showMoreDescr, setShowMoreDescr] = useState(false);
   const [showMoreCorpEstablish, setShowMoreCorpEstablish] = useState(false);
@@ -28,7 +30,7 @@ export const AboutUs: React.FC = () => {
         </div>
       </div>
 
-      <div className={cn(s.description, 'T--1-6')}>
+      <div className={cn(s.description, 'T--1-6', 'D--1-7')}>
         <div
           className={cn(s.description__text_wrapper, {
             [s.opened]: showMoreDescr,
@@ -56,6 +58,12 @@ export const AboutUs: React.FC = () => {
         </div>
       </div>
 
+      <img
+        src={Plug}
+        alt="Plug animation"
+        className={cn(s.animation, 'T--7-12', 'D--1-11')}
+      />
+
       <div
         className={cn(
           s.enterpriseDev,
@@ -80,18 +88,13 @@ export const AboutUs: React.FC = () => {
             [s.opened]: showMoreCorpEstablish,
           })}
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eos
-          repudiandae odit aspernatur repellat maiores voluptates aliquam
-          necessitatibus porro. Deserunt hic culpa alias. Debitis aliquam autem
-          quam animi sed iste culpa facere, accusamus natus asperiores eum optio
-          ab minus saepe eveniet, iusto voluptas velit ipsa ratione similique
-          illo! Eaque fugit voluptates quas numquam aut dolorem dolore ad,
-          corporis impedit totam dicta? Sint dolorum doloribus nisi vero
-          perspiciatis at adipisci laborum dolore explicabo ipsa dolor inventore
-          praesentium maxime labore nihil, quas placeat. Animi doloribus
-          asperiores voluptatem! Accusamus harum, sint dolor dolore laboriosam
-          eligendi non cum. Cupiditate eaque voluptatum consectetur culpa
-          perferendis!
+          Setting up a company in UAE / United Arab Emirates doesn’t have to be
+          complicated, we are ready to guide you through every step of the
+          process, from deciding the ideal business structure to obtaining the
+          necessary registrations and approvals. We cover an exhaustive list of
+          services to guarantee a seamless registration process from l advice on
+          deciding the most suitable business structure all way to submission of
+          necessary financial statements on yearly basis.
         </p>
 
         <div
@@ -125,18 +128,12 @@ export const AboutUs: React.FC = () => {
             [s.opened]: showMoreIntExpansion,
           })}
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eos
-          repudiandae odit aspernatur repellat maiores voluptates aliquam
-          necessitatibus porro. Deserunt hic culpa alias. Debitis aliquam autem
-          quam animi sed iste culpa facere, accusamus natus asperiores eum optio
-          ab minus saepe eveniet, iusto voluptas velit ipsa ratione similique
-          illo! Eaque fugit voluptates quas numquam aut dolorem dolore ad,
-          corporis impedit totam dicta? Sint dolorum doloribus nisi vero
-          perspiciatis at adipisci laborum dolore explicabo ipsa dolor inventore
-          praesentium maxime labore nihil, quas placeat. Animi doloribus
-          asperiores voluptatem! Accusamus harum, sint dolor dolore laboriosam
-          eligendi non cum. Cupiditate eaque voluptatum consectetur culpa
-          perferendis!
+          Global expansion service is designed to help build strong business
+          foundations in a new market. Having a clear strategy and support all
+          the way through. Expansion requires knowledge of local laws and new
+          cultural and economic realities. We will ensure the whole process runs
+          smoothly and effortlessly, meets all the local regulations, and also
+          deal with HR, financial and legal issues.
         </p>
         <div
           className={s.showMoreBtn}
@@ -169,18 +166,15 @@ export const AboutUs: React.FC = () => {
             [s.opened]: showMorePrMengm,
           })}
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eos
-          repudiandae odit aspernatur repellat maiores voluptates aliquam
-          necessitatibus porro. Deserunt hic culpa alias. Debitis aliquam autem
-          quam animi sed iste culpa facere, accusamus natus asperiores eum optio
-          ab minus saepe eveniet, iusto voluptas velit ipsa ratione similique
-          illo! Eaque fugit voluptates quas numquam aut dolorem dolore ad,
-          corporis impedit totam dicta? Sint dolorum doloribus nisi vero
-          perspiciatis at adipisci laborum dolore explicabo ipsa dolor inventore
-          praesentium maxime labore nihil, quas placeat. Animi doloribus
-          asperiores voluptatem! Accusamus harum, sint dolor dolore laboriosam
-          eligendi non cum. Cupiditate eaque voluptatum consectetur culpa
-          perferendis!
+          With an intimate knowledge of all facets of local regulations and
+          statutory requirements and long-term relationships with trusted
+          service providers and partners, assuring that the project we undertake
+          runs smoothly. Our Project Management services are the control center
+          for a project – the central point of contact for all parties involved
+          in the day-to-day activities of a project, providing a grounded and
+          informed perspective on the project, that ensures all work is carried
+          out efficiently and effectively and meets cost and quality
+          expectations.
         </p>
         <div
           className={s.showMoreBtn}
