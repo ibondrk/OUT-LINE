5206f78a5a22207e2907caffbfe8cf47b61ceee3
Igor Bondarenko <igorbondarenko35@gmail.com>
2023-06-08 16:12:01 +0300

    changed spin text

--- src/components/HeadSection/Out-line-ptoject/Out-line-project.tsx
+++ src/components/HeadSection/Out-line-ptoject/Out-line-project.tsx
@@ -4,14 +4,14 @@ import s from './out-line-project.module.scss';
 
 export const Out_line_project: React.FC = () => {
   // creating dynamic projects
-  const text = 'out-line-project-';
+  const text = ' creating dynamic projects';
   const chars = text.split('');
   const angleStep = 360 / chars.length; // angle between letters
   const angleOffset = -90; // beginning angle of rotating for text
   const containerRef = useRef<HTMLDivElement>(null);
   const [rotation, setRotation] = useState(0);
   const isSmallScreen = useMediaQuery({ maxWidth: 767 });
-  const radius = isSmallScreen ? 25 : 30; // circle radius
+  const radius = isSmallScreen ? 25 : 35; // circle radius
   useEffect(() => {
     const interval = setInterval(() => {
       setRotation((prevRotation) => prevRotation + 1);
@@ -35,10 +35,11 @@ export const Out_line_project: React.FC = () => {
         <span
           key={index}
           className={s.letter}
+          // condition just for this text. Here only for letter 'i' at the 15 place
           style={{
-            transform: `rotate(${angleStep * index}deg) translateY(-${
-              radius * 1.75
-            }px)`,
+            transform: `rotate(${
+              index === 15 ? 212.692 : angleStep * index
+            }deg) translateY(-${radius * 1.75}px)`,
             color: isSmallScreen
               ? (angleStep * index + rotation + angleOffset) % 360 > 180
                 ? 'var(--spin-text-color)'
